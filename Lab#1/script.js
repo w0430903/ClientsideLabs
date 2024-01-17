8713879cd18f6a4547af5bb98f825be06d72d0ac
Robert Halbert <113041216+w0430903@users.noreply.github.com>
2024-01-17 19:18:13 +0000

    Initial

---
+++ Lab#1/script.js
@@ -0,0 +1,8 @@
+// Access the div with the id 'output'
+const outputDiv = document.getElementById('output');
+
+// Log a message to the console
+console.log('Hello from the console!');
+
+// Display the same message on the HTML page
+outputDiv.innerText = 'Hello from the HTML!';
